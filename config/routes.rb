5ce6abed502c2a394e8f0c8026966f470c23c9ee
maria laura lucanera <mllucanera@gmail.com>
2018-06-26 13:30:41 -0300

    Add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # washer routes
+
+  resources :bookings, only: [:show, :index, :update, :edit]
+
+  # client routes
+
+  resources :bookings, only: [:new, :create]
+
+  namespace :client do
+    resources :cars
+  end
 end
