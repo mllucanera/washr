86a7444d8c71640af06e307f5c341b9bdf27c06c
maria laura lucanera <mllucanera@gmail.com>
2018-06-26 12:33:52 -0300

    Correct association in Booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :washer, foreign_key: "user_id", class_name: "User"
-  belongs_to :client, through: :car, source: :user
   belongs_to :car
+  has_one :client, through: :car, source: :user
 
   validates :car, presence: true
   validates :washer, presence: true
