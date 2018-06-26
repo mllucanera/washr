497418d88c60249d73125735d8d97bf038939f1f
maria laura lucanera <mllucanera@gmail.com>
2018-06-26 12:59:17 -0300

    Fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-luli = User.new(role: "client", first_name: "Luli", last_name: "Lucanera", phone: "2914168790", email: "mllucanera@gmail.com", password: "123456")
-joti = User.new(role: "client", first_name: "Joti", last_name: "Sempe", phone: "2914168790", email: "joti@gmail.com", password: "123456")
-jash = User.new(role: "client", first_name: "Jash", last_name: "Hash", phone: "2914168790", email: "jash@gmail.com", password: "123456")
-marcelo = User.new(role: "client", first_name: "Marcelo", last_name: "Polli", phone: "2914168790", email: "marcelo@gmail.com", password: "123456")
+luli = User.create(role: "client", first_name: "Luli", last_name: "Lucanera", phone: "2914168790", email: "mllucanera@gmail.com", password: "123456")
+joti = User.create(role: "client", first_name: "Joti", last_name: "Sempe", phone: "2914168790", email: "joti@gmail.com", password: "123456")
+jash = User.create(role: "client", first_name: "Jash", last_name: "Hash", phone: "2914168790", email: "jash@gmail.com", password: "123456")
+marcelo = User.create(role: "client", first_name: "Marcelo", last_name: "Polli", phone: "2914168790", email: "marcelo@gmail.com", password: "123456")
