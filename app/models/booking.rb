f3cf5dc754c6d7dcdd21e59706814d65cefdaf3b
maria laura lucanera <mllucanera@gmail.com>
2018-06-26 11:51:02 -0300

    Create validation for car and booking models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :washer, foreign_key: "user_id", class_name: "User"
   belongs_to :client, through: :car, source: :user
   belongs_to :car
+
+  validates :car, presence: true
+  validates :washer, presence: true
+
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,12 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+
+  validates :user, presence: true
+  validates :name, presence: true
+  validates :plate, presence: true, uniqueness: true
+  validates :brand, presence: true
+  validates :color, presence: true
+  validates :photo, presence: true
+
 end
