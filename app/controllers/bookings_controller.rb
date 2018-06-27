5747e1ec34dc45846d96fa59bb36520367fb9dca
Jash Shah <jash.shah@live.com>
2018-06-27 17:18:01 -0300

    Create map with pin and reach booking#new.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,10 +4,16 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
+    if Car.all.empty?
+
+    else
+
+    end
   end
 
   def create
     @booking = Booking.new(booking_params)
+    @booking.address = address
     @booking.user = current_user
     if @booking.save
       redirect_to booking_path(@booking)
@@ -40,4 +46,6 @@ class BookingsController < ApplicationController
   def booking_params
     params.require[:booking].permit[:car, :address]
   end
+
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,18 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    if params[:address].present?
+      address = params[:address]
+      pin_address(address)
+    end
+  end
+
+  def pin_address(address)
+    res = Geocoder.search(address)
+    @markers =
+    [{
+      lat: res.first.coordinates[0],
+      lng: res.first.coordinates[1]
+      }]
   end
 end
