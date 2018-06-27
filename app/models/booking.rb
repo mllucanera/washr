5c7e3ee4640ec1aa27e96af194a26f096f30cf89
joti sempe <josefinasempe@gmail.com>
2018-06-27 14:24:19 -0300

    Install Geocoder and Places API

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,7 @@ class Booking < ApplicationRecord
   has_one :client, through: :car, source: :user
 
   validates :car, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
