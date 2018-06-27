50fc818edcd9c45bc97b3ee1dc06575d176b5776
Jash Shah <jash.shah@live.com>
2018-06-27 20:07:32 -0300

    Have current location on home.

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,90 +1,102 @@
 import GMaps from 'gmaps/gmaps.js';
+console.log(mapElement);
+console.log("mapElement");
 
 const mapElement = document.getElementById('map');
-if (mapElement) { // don't try to build a map if there's no div#map to inject in
-  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
-  const markers = JSON.parse(mapElement.dataset.markers);
-  map.addMarkers(markers);
-  if (markers.length === 0) {
-    map.setZoom(2);
-  } else if (markers.length === 1) {
+// don't try to build a map if there's no div#map to inject in
+const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+const markers = JSON.parse(mapElement.dataset.markers);
+map.addMarkers(markers);
+if (markers.length === 1) {
     map.setCenter(markers[0].lat, markers[0].lng);
     map.setZoom(18);
-  } else {
-    map.fitLatLngBounds(markers);
   }
-}
+   else {
+    navigator.geolocation.getCurrentPosition(function(location) {
+    var lat = location.coords.latitude;
+    var lng = location.coords.longitude;
+    map.setCenter(lat, lng);
+    map.setZoom(15);
+     })
+  }
+
+
+
+
+// don't try to build a map if there's no div#map to inject in
+
+
 
 const styles = [
-    {
-        "featureType": "landscape.natural",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "color": "#e0efef"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "hue": "#1900ff"
-            },
-            {
-                "color": "#c0e8e8"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "lightness": 100
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.line",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "lightness": 700
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "all",
-        "stylers": [
-            {
-                "color": "#7dcdcd"
-            }
-        ]
-    }
+{
+  "featureType": "landscape.natural",
+  "elementType": "geometry.fill",
+  "stylers": [
+  {
+    "visibility": "on"
+  },
+  {
+    "color": "#e0efef"
+  }
+  ]
+},
+{
+  "featureType": "poi",
+  "elementType": "geometry.fill",
+  "stylers": [
+  {
+    "visibility": "on"
+  },
+  {
+    "hue": "#1900ff"
+  },
+  {
+    "color": "#c0e8e8"
+  }
+  ]
+},
+{
+  "featureType": "road",
+  "elementType": "geometry",
+  "stylers": [
+  {
+    "lightness": 100
+  },
+  {
+    "visibility": "simplified"
+  }
+  ]
+},
+{
+  "featureType": "road",
+  "elementType": "labels",
+  "stylers": [
+  {
+    "visibility": "off"
+  }
+  ]
+},
+{
+  "featureType": "transit.line",
+  "elementType": "geometry",
+  "stylers": [
+  {
+    "visibility": "on"
+  },
+  {
+    "lightness": 700
+  }
+  ]
+},
+{
+  "featureType": "water",
+  "elementType": "all",
+  "stylers": [
+  {
+    "color": "#7dcdcd"
+  }
+  ]
+}
 ];
 
 map.addStyle({
