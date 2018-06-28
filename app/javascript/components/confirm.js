913300641d0462ba5efdb68b8891765e1c09a3cf
Jash Shah <jash.shah@live.com>
2018-06-28 16:30:40 -0300

    Makes minor corrections

--- app/javascript/components/confirm.js
+++ app/javascript/components/confirm.js
@@ -9,28 +9,12 @@ function bindSweetAlertButtonDemo() {
         text: "This is a great alert, isn't it?",
         icon: "success"
 
-      }, function( => {
-        window.location.href = "views/booking/new.html";
-        showCancelButton: true
-      });
-      console.log("Reached")
-      // warnBeforeRedirect('views/booking/new.html')
+      }
     });
-    }
+    };
   }
 
-// function warnBeforeRedirect(new_booking_path) {
-//     swal({
-//       title: "Leave this site?",
-//       text: "If you click 'OK', you will be redirected to " + new_booking_path,
-//       type: "warning",
-//     }, function() {
-//       // Redirect the user
-//       window.location.href = new_booking_path;
-//       showCancelButton: true
-//     });
-//   }
 
 
 
-export { bindSweetAlertButtonDemo };
+  export { bindSweetAlertButtonDemo };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,3 @@
 import "bootstrap";
+import { bindSweetAlertButtonDemo } from '../components/confirm';
+bindSweetAlertButtonDemo();
