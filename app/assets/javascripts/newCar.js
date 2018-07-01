fec803b2b1e359c7afd788080f398de8c6a44512
Jash Shah <jash.shah@live.com>
2018-07-01 20:27:18 -0300

    Solve conflicts

--- app/assets/javascripts/newCar.js
+++ app/assets/javascripts/newCar.js
@@ -1,10 +1,24 @@
-const element = document.querySelector("hidden");
-element.addEventListener("click", (event) => {
-  console.log(event);
-  console.log(event.currentTarget);
-  element.classList.remove("hidden");
-});
+const addCarIndex = document.getElementById("add-car-index");
+const formSubmit = document.getElementById("form-submit");
+const newCarIndex = document.getElementById("new-car-index");
+const index = document.getElementById("index");
+
+if (index) {
 
 
-// element.classList.add("hidden");
+  addCarIndex.addEventListener("click", (event) => {
+    newCarIndex.classList.remove("hidden");
+  });
+  formSubmit.addEventListener("click", (event) => {
+    newCarIndex.classList.add("hidden");
+  });
+}
 
+const addCarBooking = document.getElementById("add-car-booking");
+if (addCarBooking)
+{
+  addCarBooking.addEventListener("click", (event) => {
+    newCarBooking = document.getElementById("new-car-booking");
+    newCarBooking.classList.remove("hidden");
+  });
+}
