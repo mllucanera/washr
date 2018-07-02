ef49e87937e57287112133392f37b668a182184e
maria laura lucanera <mllucanera@gmail.com>
2018-07-02 19:07:59 -0300

    Add a marker on the current location

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,93 +1,13 @@
 import GMaps from 'gmaps/gmaps.js';
-import { autocomplete } from '../components/autocomplete';
-
-autocomplete();
 
 const mapElement = document.getElementById('map');
 // don't try to build a map if there's no div#map to inject in
 const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
-const styles = [
-{
-  "featureType": "landscape.natural",
-  "elementType": "geometry.fill",
-  "stylers": [
-  {
-    "visibility": "on"
-  },
-  {
-    "color": "#e0efef"
-  }
-  ]
-},
-{
-  "featureType": "poi",
-  "elementType": "geometry.fill",
-  "stylers": [
-  {
-    "visibility": "on"
-  },
-  {
-    "hue": "#1900ff"
-  },
-  {
-    "color": "#c0e8e8"
-  }
-  ]
-},
-{
-  "featureType": "road",
-  "elementType": "geometry",
-  "stylers": [
-  {
-    "lightness": 100
-  },
-  {
-    "visibility": "simplified"
-  }
-  ]
-},
-{
-  "featureType": "road",
-  "elementType": "labels",
-  "stylers": [
-  {
-    "visibility": "off"
-  }
-  ]
-},
-{
-  "featureType": "transit.line",
-  "elementType": "geometry",
-  "stylers": [
-  {
-    "visibility": "on"
-  },
-  {
-    "lightness": 700
-  }
-  ]
-},
-{
-  "featureType": "water",
-  "elementType": "all",
-  "stylers": [
-  {
-    "color": "#7dcdcd"
-  }
-  ]
-}
-];
-
 const markers = JSON.parse(mapElement.dataset.markers);
 if (markers.length === 1) {
   map.setCenter(markers[0].lat, markers[0].lng);
   map.setZoom(18);
-  map.addMarker(marker);
-  map.addStyle({
-    styles: styles,
-    mapTypeId: 'map_style'
-  });
-  map.setStyle('map_style');
+  map.addMarkers(markers);
 }
 else {
   navigator.geolocation.getCurrentPosition(function(location) {
@@ -102,13 +22,5 @@ else {
       icon: 'http://res.cloudinary.com/sebastianbuffosempe/image/upload/v1530550811/carwasher-icon_g7ojz7.png'
     });
     map.addMarker(marker);
-    map.addStyle({
-      styles: styles,
-      mapTypeId: 'map_style'
-    });
-    map.setStyle('map_style');
-  });
+  })
 }
-
-
-
