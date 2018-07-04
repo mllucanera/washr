49da4529fee356f2b556f5a16f20479b8acf140f
Jash Shah <jash.shah@live.com>
2018-07-04 18:44:17 -0300

    Add rating to wash

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,11 @@ import "bootstrap";
 import { btnGooFunc, inputBlur } from "./btnGoo";
 import { autocomplete } from "../components/autocomplete";
 import { selectCar } from "./selectCar";
+import { rate } from "./review";
 
 
 btnGooFunc();
 // inputBlur();
 autocomplete();
 selectCar();
+rate();
---
+++ app/javascript/packs/review.js
@@ -0,0 +1,17 @@
+
+const stars = document.querySelectorAll(".fa-star");
+
+
+stars.forEach(star => {
+  star.addEventListener("click", (event) => {
+    for (i = 0; i < 5; i++) {
+      stars[i].classList.remove("yellow");
+    }
+    for (i = 0; i <= star.id; i++) {
+      stars[i].classList.add("yellow");
+    }
+    for (i = star.id + 1; i < 5; i++) {
+      stars[i].classList.remove("yellow");
+    }
+  });
+});
