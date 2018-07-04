ca408ddc405448b516061e49c0a2a4711b0946c0
samuel <samulevy@gmail.com>
2018-07-04 13:04:27 -0300

    Select car

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,10 @@
 import "bootstrap";
 import { btnGooFunc, inputBlur } from "./btnGoo";
 import { autocomplete } from "../components/autocomplete";
+import { selectCar } from "./selectCar";
 
 
 btnGooFunc();
 // inputBlur();
 autocomplete();
+selectCar();
