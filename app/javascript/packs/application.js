3c70f32ff2e7d0ba7e6861f197027ccb309285aa
Jash Shah <jash.shah@live.com>
2018-07-04 10:58:26 -0300

    Enable 'Park it.' text

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,13 @@
 import "bootstrap";
 import { btnGooFunc, inputBlur } from "./btnGoo";
 import { autocomplete } from "../components/autocomplete";
-
+    console.log("IN APP JS before");
+import { loadDynamicBannerText } from '../components/slogan';
+    console.log("IN APP JS after");
 
 btnGooFunc();
 // inputBlur();
 autocomplete();
+
+// Park it.
+loadDynamicBannerText();
